a413f02bd73ce21ab442f538308771218d0604ac
Futjan sup <abdulwahabdev0@gmail.com>
2023-01-17 21:13:37 +0500

    remove posts record from schema

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -13,28 +13,6 @@ async function main() {
       name: "test1",
       id: 1,
       password: "12345678",
-      profile: {
-        create: {
-          id: 1,
-          bio: "I'm a programmer",
-        },
-      },
-      posts: {
-        create: [
-          {
-            id: 1,
-            title: "its my first post",
-            content: "I'm tring to learn relation db",
-            published: true,
-          },
-          {
-            id: 2,
-            title: "Follow Nexus on Twitter",
-            content: "https://twitter.com/nexusgql",
-            published: true,
-          },
-        ],
-      },
     },
   });
 }
